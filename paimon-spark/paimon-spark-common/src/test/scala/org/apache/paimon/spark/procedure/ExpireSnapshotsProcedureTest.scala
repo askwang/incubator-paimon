d2727b401bcda33aaf2f478de8db7ef7fcdaac18
Askwang <135721692+Askwang@users.noreply.github.com>
2024-07-16 09:06:50 +0800

    ExpireSnapshotsProcedureTest

--- paimon-spark/paimon-spark-common/src/test/scala/org/apache/paimon/spark/procedure/ExpireSnapshotsProcedureTest.scala
+++ paimon-spark/paimon-spark-common/src/test/scala/org/apache/paimon/spark/procedure/ExpireSnapshotsProcedureTest.scala
@@ -136,4 +136,86 @@ class ExpireSnapshotsProcedureTest extends PaimonSparkTestBase with StreamTest {
       }
     }
   }
+
+  // min maybe large than maxExclusive
+  test("Paimon Procedure: max delete") {
+    failAfter(streamingTimeout) {
+      withTempDir {
+        checkpointDir =>
+          // define a change-log table and test `forEachBatch` api
+          spark.sql(s"""
+                       |CREATE TABLE T (a INT, b STRING)
+                       |TBLPROPERTIES ('primary-key'='a', 'bucket'='3', 'write-only'='true')
+                       |""".stripMargin)
+          val location = loadTable("T").location().toString
+
+          val inputData = MemoryStream[(Int, String)]
+          val stream = inputData
+            .toDS()
+            .toDF("a", "b")
+            .writeStream
+            .option("checkpointLocation", checkpointDir.getCanonicalPath)
+            .foreachBatch {
+              (batch: Dataset[Row], _: Long) =>
+                batch.write.format("paimon").mode("append").save(location)
+            }
+            .start()
+
+          val query = () => spark.sql("SELECT * FROM T ORDER BY a")
+
+          try {
+            // snapshot-1
+            inputData.addData((1, "a"))
+            stream.processAllAvailable()
+            checkAnswer(query(), Row(1, "a") :: Nil)
+
+            // snapshot-2
+            inputData.addData((2, "b"))
+            stream.processAllAvailable()
+            checkAnswer(query(), Row(1, "a") :: Row(2, "b") :: Nil)
+
+            // snapshot-3
+            inputData.addData((2, "b2"))
+            stream.processAllAvailable()
+            checkAnswer(query(), Row(1, "a") :: Row(2, "b2") :: Nil)
+
+            // snapshot-4
+            inputData.addData((4, "b"))
+            stream.processAllAvailable()
+
+            // snapshot-5
+            inputData.addData((5, "b"))
+            stream.processAllAvailable()
+
+            // snapshot-6
+            inputData.addData((6, "b"))
+            stream.processAllAvailable()
+
+            // snapshot-7
+            inputData.addData((7, "b"))
+            stream.processAllAvailable()
+
+            // snapshot-8
+            inputData.addData((8, "b"))
+            stream.processAllAvailable()
+
+            // snapshot-9
+            inputData.addData((9, "b"))
+            stream.processAllAvailable()
+
+            // snapshot-10
+            inputData.addData((10, "b"))
+            stream.processAllAvailable()
+
+            // expire assert throw exception
+            assertThrows[IllegalArgumentException] {
+              spark.sql(
+                "CALL paimon.sys.expire_snapshots(table => 'test.T', retain_max => 5, retain_min => 2, max_deletes => 4)")
+            }
+          } finally {
+            stream.stop()
+          }
+      }
+    }
+  }
 }
